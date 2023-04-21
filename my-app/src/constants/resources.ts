58c1efc8b05c2a3be7fd8bb81355238533a7c56b
siny <dltlsgh5@gmail.com>
2023-04-21 20:54:45 +0900

    [FEAT] resources.ts 추가

---
+++ my-app/src/constants/resources.ts
@@ -0,0 +1,8 @@
+export const resources: Record<string, string> = {
+  logo: './logo.svg',
+  swap: './swap.svg',
+  solana: './coinIcons/solana.svg',
+  ethereum: './coinIcons/ethereum.svg',
+  bnb: './coinIcons/bnb.svg',
+  right: './right.svg',
+};
